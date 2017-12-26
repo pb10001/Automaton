6fe70f7c1fa66da96c4ce5180781710f7799f40e
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2017-12-26 09:12:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Automaton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
